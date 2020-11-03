30045afc4edf922cb7ae190daa12ce187221d415
Rafael Madakis <rafaelmadakis@gmail.com>
2020-11-03 10:17:53 -0300

    Finalizando app que demonstra passagem de dados de uma activity para outra

--- app/src/main/java/com/example/caraoucoroa/ResultadoActivity.java
+++ app/src/main/java/com/example/caraoucoroa/ResultadoActivity.java
@@ -3,12 +3,39 @@ package com.example.caraoucoroa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ResultadoActivity extends AppCompatActivity {
+
+    private ImageView buttonVoltar;
+    private ImageView imageResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        buttonVoltar = findViewById(R.id.buttonVoltar);
+        imageResultado = findViewById(R.id.imageResultado);
+
+        //recuperar dados
+        Bundle dados = getIntent().getExtras();
+        int numero = dados.getInt("numero");
+
+        if (numero == 0) {
+            imageResultado.setImageResource(R.drawable.moeda_cara);
+        }else{
+            imageResultado.setImageResource(R.drawable.moeda_coroa);
+        }
+
+        //evento de click para o botão
+        buttonVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); //Finaliza esta activity
+            }
+        });
+
     }
 }
